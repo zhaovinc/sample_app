43ffba126ed09621bd8d98246a3ff202974d2e5d
zegal <ravensore@gmail.com>
2010-07-26 23:37:30 +0800

    done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,15 @@
 class PagesController < ApplicationController
   def home
+    @title = "home"
   end
 
   def contact
+    @title = "contact"
   end
 
+  def about
+    @title = "about"
+  end
+  
+  
 end
